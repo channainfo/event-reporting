26585a6fa92b7421a730da9ca101a6f432e685b0
kimsanlim <kimsan@camtasia-tech.com>
2016-03-22 16:44:06 +0700

    [Finish #116098095] fixed locations always clear after user sign out

--- www/js/services/store_service.js
+++ www/js/services/store_service.js
@@ -9,7 +9,7 @@
                 return localStorage[name]
               },
               clear: function(name){
-                localStorage.clear(name)
+                localStorage.removeItem(name)
               },
               isSet: function(name){
                 return localStorage[name] != undefined
