f53a20d40dac16ef348a55fd216753e559b4101b
kimsanlim <kimsan@camtasia-tech.com>
2016-03-30 11:51:48 +0700

    changed variable name to camelcase in day_interval.js

--- www/js/lib/avocado/day_interval.js
+++ www/js/lib/avocado/day_interval.js
@@ -11,8 +11,8 @@ avocado.dayInterval = {
     return (dateObj.getHours() >= 0 && dateObj.getHours() < 6) || (dateObj.getHours() >= 18 && dateObj.getHours() <= 23) ? true : false;
   },
 
-  isHourFromNow: function(departure_time, hour_in_sec) {
-    var current_time_in_sec = new Date().getSeconds() + (60 * new Date().getMinutes()) + (60 * 60 * new Date().getHours());
-    return departure_time - current_time_in_sec < hour_in_sec;
+  isHourFromNow: function(departureTime, hourInSec) {
+    var currentTimeInSec = new Date().getSeconds() + (60 * new Date().getMinutes()) + (60 * 60 * new Date().getHours());
+    return departureTime - currentTimeInSec < hourInSec;
   }
 }
