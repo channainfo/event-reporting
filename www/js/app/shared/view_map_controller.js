2347f85195f07b6414011ff4ce0963f01aec08af
kimsanlim <kimsan@camtasia-tech.com>
2016-04-25 14:46:13 +0700

    [Finish #118262209] implemented google maps plugin cordova

--- www/js/app/shared/view_map_controller.js
+++ www/js/app/shared/view_map_controller.js
@@ -1,31 +1,34 @@
 (function(){
   var viewMapController = function(){
-    this.map = {
-      center: { latitude: 11.575961, longitude: 104.925362 },
-      zoom: 15
-    }
-
-    this.marker = [
-      {
-        id: 1,
-        latitude: 11.575961,
-        longitude: 104.925362
-      },
-      {
-        id: 2,
-        latitude: 11.578774,
-        longitude: 104.924366
-      },
-      {
-        id: 3,
-        latitude: 11.574907,
-        longitude: 104.927049
-      }
+    var map
+    var locations = [
+      { lat: 11.575961, lng: 104.925362, title: "#103 Eo, Sisowath Street, S.K Wat Phnom, Khan Daun Penh, Phnom Penh City, Kingdom of Cambodia" },
+      { lat: 11.578774, lng: 104.924366, title: "#2020 , No Road 5, S.K Toul Sangke, Khan Russey Keo, Phnom Penh" }
     ]
+    var location = new plugin.google.maps.LatLng(locations[0].lat, locations[0].lng)
 
-    this.option = {
-      draggable: false
-    }
+    document.addEventListener("deviceready", function() {
+      var element = document.querySelector('.mapCanvas')
+      // Initialize the map view
+      map = plugin.google.maps.Map.getMap(element, {
+        'camera': {
+          'latLng': location,
+          'zoom': 15
+        }
+      })
+
+      map.addEventListener(plugin.google.maps.event.MAP_READY, function() {
+        for(var i = 0; i<locations.length; i++){
+          var locObj = new plugin.google.maps.LatLng(locations[i].lat,locations[i].lng);
+          map.addMarker({
+            'position': locObj,
+            'title': locations[i].title
+          }, function(marker) {
+            marker.hideInfoWindow()
+          })
+        }
+      })
+    }, false);
   }
 
   angular.module('bookmebus')
--- www/js/bookmebus_module.js
+++ www/js/bookmebus_module.js
@@ -1,3 +1,3 @@
 angular.module("bookmebus", ["cordovaAngularEvent", "ngMaterial", "ngMessages",
                              "ui.router", "internationalPhoneNumber", "ngMdIcons",
-                             "angularMoment", "slick", "disableAll", "uiGmapgoogle-maps"]);
+                             "angularMoment", "slick", "disableAll"]);
